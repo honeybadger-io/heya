17d0c8563104f71ba984a69e74ae16bdd836f3ac
Josh Wood <josh@joshuawood.net>
2019-07-03 13:06:59 -0700

    Add default_segment to Contact (#8)

--- lib/heya/concerns/models/contact.rb
+++ lib/heya/concerns/models/contact.rb
@@ -4,12 +4,24 @@ module Heya
       module Contact
         extend ActiveSupport::Concern
 
+        included do
+          class_attribute :__heya_default_segment, instance_writer: true, instance_predicate: false, default: nil
+        end
+
         module ClassMethods
           # Segments are just scopes, added via the ::segment method (currently
           # just an alias, but I may store these in the future in order to
           # track membership).
           def segment(name, scope_lambda)
             scope(name, scope_lambda)
+          end
+
+          def default_segment(&block)
+            self.__heya_default_segment = block
+          end
+
+          def build_default_segment(relation = relation()) # rubocop:disable Style/MethodCallWithoutArgsParentheses
+            __heya_default_segment && relation.instance_exec(&__heya_default_segment) || relation
           end
         end
       end
---
+++ test/lib/heya/concerns/models/contact_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+module Heya
+  module Models
+    module Concerns
+      class ContactTest < ActiveSupport::TestCase
+        include ActionMailer::TestHelper
+
+        class DefaultSegmentContact < Contact
+          default_segment { where(email: "one@example.com") }
+        end
+
+        test "has a default segment of all" do
+          assert_equal [contacts(:one), contacts(:two)], Contact.build_default_segment.limit(2).to_a
+        end
+
+        test "can override the default segment via lambda" do
+          assert_equal [contacts(:one)], (DefaultSegmentContact.build_default_segment.limit(2).to_a.map { |m| m.becomes(Contact) })
+        end
+      end
+    end
+  end
+end
