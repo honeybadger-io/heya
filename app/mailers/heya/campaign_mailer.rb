b2bc851dcfc1386206796ed012fc5092ec4c3afa
Joshua Wood <josh@joshuawood.net>
2019-11-22 16:13:51 -0800

    Name environment variable after contact.

--- app/mailers/heya/campaign_mailer.rb
+++ app/mailers/heya/campaign_mailer.rb
@@ -2,13 +2,15 @@ class Heya::CampaignMailer < ApplicationMailer
   default from: "badgers@honeybadger.io"
 
   def build
-    @contact = params.fetch(:contact)
+    contact = params.fetch(:contact)
 
     message = params.fetch(:message)
     campaign = message.campaign
 
+    instance_variable_set(:"@#{contact.model_name.element}", contact)
+
     mail(
-      to: @contact.email,
+      to: contact.email,
       subject: message.properties.fetch("subject"),
       template_path: "heya/campaign_mailer/#{campaign.name.underscore}",
       template_name: message.name.underscore
