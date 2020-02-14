f9eecb057b563286ef5f71cc0c27e475c4d3c49b
Joshua Wood <josh@joshuawood.net>
2020-02-14 09:06:28 -0800

    Use migration generator instead of rake
    This method automatically cleans up generated test files.

--- lib/generators/heya/install/install_generator.rb
+++ lib/generators/heya/install/install_generator.rb
@@ -1,11 +1,18 @@
 class Heya::InstallGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+
   source_root File.expand_path("templates", __dir__)
 
   def copy_migrations
-    rake "heya:install:migrations"
+    migration_template "migration.rb", "db/migrate/create_heya_tables.rb"
   end
 
   def copy_initializer_file
     copy_file "initializer.rb", "config/initializers/heya.rb"
+  end
+
+  def self.next_migration_number(dirname)
+    next_migration_number = current_migration_number(dirname) + 1
+    ActiveRecord::Migration.next_migration_number(next_migration_number)
   end
 end
