18e6100e2a63741f02995a3b4c19c9aac4bf79dd
Joshua Wood <josh@joshuawood.net>
2020-02-13 21:07:36 -0800

    Add block actions
    When steps have a block, build an action which always calls the block
    synchronously (no background processing); the block is responsible for
    delaying execution if necessary.

--- lib/heya/campaigns/actions.rb
+++ lib/heya/campaigns/actions.rb
@@ -5,5 +5,27 @@ module Heya
         .with(user: user, step: step)
         .build
     end
+
+    class Block
+      class Execution; end
+
+      def self.build(block)
+        ->(user:, step:) {
+          new(user, step, block)
+        }
+      end
+
+      def initialize(user, step, block)
+        @user = user
+        @step = step
+        @block = block
+        @execution = Execution.new
+      end
+
+      def deliver_now
+        @execution.instance_exec(@user, @step, &@block)
+      end
+      alias deliver_later deliver_now
+    end
   end
 end
