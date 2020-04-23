b5bd1af3bfb73842cae30fe6fda15f5fcf5e9a0c
Joshua Wood <josh@joshuawood.net>
2020-04-23 12:02:23 -0700

    Delete old file

--- test/mailers/previews/hiya/campaign_mailer_preview.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Heya
-  # Preview all emails at http://localhost:3000/rails/mailers/campaign_mailer
-  class CampaignMailerPreview < ActionMailer::Preview
-  end
-end
