ea6ff09c290181c74c1de4234e48bff6dec49887
Joshua Wood <josh@joshuawood.net>
2020-10-10 15:00:50 -0700

    format

--- app/models/heya/campaign_membership.rb
+++ app/models/heya/campaign_membership.rb
@@ -9,15 +9,15 @@ module Heya
     def self.migrate_next_step!
       find_each do |membership|
         campaign = GlobalID::Locator.locate(membership.campaign_gid)
-        receipt = campaign && CampaignReceipt.where(user: membership.user, step_gid: campaign.steps.map(&:gid)).order('created_at desc').first
+        receipt = campaign && CampaignReceipt.where(user: membership.user, step_gid: campaign.steps.map(&:gid)).order("created_at desc").first
 
         next_step = if receipt
-                      last_step = GlobalID::Locator.locate(receipt.step_gid)
-                      current_index = campaign.steps.index(last_step)
-                      campaign.steps[current_index + 1]
-                    else
-                      campaign&.steps&.first
-                    end
+          last_step = GlobalID::Locator.locate(receipt.step_gid)
+          current_index = campaign.steps.index(last_step)
+          campaign.steps[current_index + 1]
+        else
+          campaign&.steps&.first
+        end
 
         if next_step
           membership.update(step_gid: next_step.gid)
