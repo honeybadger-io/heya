fbdf141653d512c722c5d3001eef5b754b80a890
Josh Wood <josh@joshuawood.net>
2022-07-07 13:49:23 -0700

    Reproduce bug in #177

--- test/models/heya/contact_test.rb
+++ test/models/heya/contact_test.rb
@@ -4,24 +4,5 @@ require "test_helper"
 
 module Heya
   class ContactTest < ActiveSupport::TestCase
-    test "it deletes campaign memberships on destroy" do
-      contact = contacts(:one)
-      CampaignMembership.create(user: contact, campaign_gid: "foo", step_gid: "bar")
-      memberships = CampaignMembership.where(user_type: "Contact", user_id: contact.id)
-
-      contact.destroy
-
-      assert memberships.empty?
-    end
-
-    test "it deletes campaign receipts on destroy" do
-      contact = contacts(:one)
-      CampaignReceipt.create(user: contact, step_gid: "foo")
-      receipts = CampaignReceipt.where(user_type: "Contact", user_id: contact.id)
-
-      contact.destroy
-
-      assert receipts.empty?
-    end
   end
 end
