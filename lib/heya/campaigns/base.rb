0bfac4a178f9e707d3895d9b6b89968f339ebd5f
montekaka <josh.chen.jia@gmail.com>
2023-03-21 16:12:55 -0700

    removed the inherted

--- lib/heya/campaigns/base.rb
+++ lib/heya/campaigns/base.rb
@@ -7,7 +7,7 @@ module Heya
   module Campaigns
     # {Campaigns::Base} provides a Ruby DSL for building campaign sequences.
     # Multiple actions are supported; the default is email.
-    class Base < ActionMailer::Base
+    class Base
       extend ActiveSupport::DescendantsTracker
 
       include Singleton
