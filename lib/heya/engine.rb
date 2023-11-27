14c93cb7c2a4303a25467fcf531cce4849816839
Joshua Wood <josh@joshuawood.net>
2023-11-27 09:34:28 -0800

    Fix a Rails startup bug when validating steps that use translations from locale files (#216)
    * Fix a Rails startup bug when validating steps that use translations from locale files.
    
    * Update CHANGELOG.md [skip ci]

--- lib/heya/engine.rb
+++ lib/heya/engine.rb
@@ -11,6 +11,18 @@ module Heya
     end
 
     config.to_prepare do
+      # This runs the first time *before* Rails is initialized. Because
+      # campaigns depend on Rails initialization, we use the `after_initialize`
+      # hook when Rails first starts up, and then `to_prepare` when reloading
+      # in development. See https://github.com/honeybadger-io/heya/issues/211
+      if ::Rails.application.initialized?
+        Dir.glob(Rails.root + "app/campaigns/*.rb").each do |c|
+          require_dependency(c)
+        end
+      end
+    end
+
+    config.after_initialize do
       Dir.glob(Rails.root + "app/campaigns/*.rb").each do |c|
         require_dependency(c)
       end
