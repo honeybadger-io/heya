1ded219c4d46a52689da162c4c52a4060a6a4875
HenriqueMorato <riquemorato@gmail.com>
2024-10-14 10:53:20 -0300

    Skip views and preview flags

--- test/lib/generators/heya/heya/campaign_generator_test.rb
+++ test/lib/generators/heya/heya/campaign_generator_test.rb
@@ -11,8 +11,26 @@ module Heya
 
     test "generator runs without errors" do
       assert_nothing_raised do
-        run_generator ["arguments"]
+        run_generator %w[arguments welcome:0]
       end
+      assert_file "test/mailers/previews/arguments_campaign_preview.rb"
+      assert_file "app/views/heya/campaign_mailer/arguments_campaign/welcome.html.erb"
+      assert_file "app/views/heya/campaign_mailer/arguments_campaign/welcome.text.erb"
+    end
+
+    test "supports flag --skip-previews" do
+      assert_nothing_raised do
+        run_generator %w[arguments --skip-previews]
+      end
+      assert_no_file "test/mailers/previews/arguments_campaign_preview.rb"
+    end
+
+    test "supports flag --skip-views" do
+      assert_nothing_raised do
+        run_generator %w[arguments welcome:0 --skip-views]
+      end
+      assert_no_file "app/views/heya/campaign_mailer/arguments_campaign/welcome.html.erb"
+      assert_no_file "app/views/heya/campaign_mailer/arguments_campaign/welcome.text.erb"
     end
   end
 end
